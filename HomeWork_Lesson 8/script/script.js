c44fa8dc64f9fc18eed86d84638e3c328db8aa86
OleksandrNHM <133905345+OleksandrNHM@users.noreply.github.com>
2023-09-27 07:19:46 +0200

    Add files via upload

---
+++ HomeWork_Lesson 8/script/script.js
@@ -0,0 +1,32 @@
+// console.log("старт");
+// for (let i = 1; i <= 9; i++) {
+//     console.log(i);
+// }
+// console.log("финиш");
+
+// for (let i = 10; i <= 99; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// const numbers = [10, 20, 5, 15, 30, 25];
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] >= 15) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+// const numbers = [10, 15, 7, 24, 31, 16];
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 !== 0) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+// const values = [42, 17, 68, 93, 25, 11, 7, 36];
+// for (let i = 0; i < values.length; i++) {
+//     if (i % 3 === 0) {
+//         console.log(values[i]);
+//     }
+// }
